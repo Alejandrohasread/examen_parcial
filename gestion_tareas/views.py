e8559a1b0c66f7013656604b3b74d7ed35f6b43d
Rodrigo Flores <rafloresz@pucp.edu.pe>
2022-11-26 13:47:49 -0500

    8vo commit: Se llenaron las bases de datos de las tareas iniciales en la vista dashboard y del usuario de la aplicación, almacenandolos en sqlite3, también se creo la opción de filtrado de tareas por usuario designado en la vista de dashboard

--- gestion_tareas/views.py
+++ gestion_tareas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import usuario, tarea
 # Create your views here.
-lista_tareas =[['Lavar el baño','Limpiar el inodoro y el piso de la ducha','2022-11-25','2022-11-25','2022-11-25','Rosario'],['Pasear al perro','Sacar al perro consu correa por los alrededores y esperar a que haga sus necesidades','2022-04-06','2022-11-25','Rodrigo'],['Barrer la sala','Usar la escoba y el recogedor para limpiar la superficie del piso de la sala de tal forma que quede libre de polvo','2022-11-20','2022-11-25','ALvaro']]
 
 def index(request):
     return HttpResponse('Esta es mi primera vista')  
@@ -9,34 +9,37 @@ def index(request):
 #Aqui estan el usuario y la contraseña
 
 def login(request):
+    usuariosInformacion = usuario.objects.all().order_by('id')
     return render(request,'gestion_tareas/login.html',{
-        'usuario': 'rafloresz@pucpe.edu.pe',
-        'contraseña': 'levi',
-
-    })
+        'usuarios': usuariosInformacion,
+        })
 def dashboard(request):
+    tareasInformacion = tarea.objects.all().order_by('id')
     if request.method == 'POST':
-        nuevaTarea = []
-        print('Hola, se ha posteado la información ')
-        titulo = request.POST.get('tituloTarea')
-        descripcion = request.POST.get('descripcionTarea')
-        fecha_creacion = request.POST.get('creacionTarea')
-        fecha_entrega = request.POST.get('entregaTarea')
-        usuario_designado = request.POST.get('designadoTarea')
-        print('El título de la tarea es: ' + str(titulo))
-        print('La descripción de la tarea es: ' + str(descripcion))
-        print('La fecha de creación de la tarea es: ' + str(fecha_creacion))
-        print('La fecha de entrega de la tarea es: ' + str(fecha_entrega))
-        print('El usuario designado de la tarea es: ' + str(usuario_designado))    
-        nuevaTarea.append(str(titulo))
-        nuevaTarea.append(str(descripcion))
-        nuevaTarea.append(str(fecha_creacion))
-        nuevaTarea.append(str(fecha_entrega))
-        nuevaTarea.append(str(usuario_designado))
-        lista_tareas.append(nuevaTarea)   
-     
-   
+        if 'Crear' in request.POST:
+            nuevaTarea = []
+            print('Hola, se ha posteado la información ')
+            titulo = request.POST.get('tituloTarea')
+            descripcion = request.POST.get('descripcionTarea')
+            fecha_creacion = request.POST.get('creacionTarea')
+            fecha_entrega = request.POST.get('entregaTarea')
+            usuario_designado = request.POST.get('designadoTarea')
+            print('El título de la tarea es: ' + str(titulo))
+            print('La descripción de la tarea es: ' + str(descripcion))
+            print('La fecha de creación de la tarea es: ' + str(fecha_creacion))
+            print('La fecha de entrega de la tarea es: ' + str(fecha_entrega))
+            print('El usuario designado de la tarea es: ' + str(usuario_designado))    
+            nuevaTarea.append(str(titulo))
+            nuevaTarea.append(str(descripcion))
+            nuevaTarea.append(str(fecha_creacion))
+            nuevaTarea.append(str(fecha_entrega))
+            nuevaTarea.append(str(usuario_designado))
+            tarea(titulo = str(titulo),descripcion = str(descripcion),fecha_creacion = str(fecha_creacion),fecha_entrega = str(fecha_entrega),usuario_designado = str(usuario_designado)).save()
+            tareasInformacion = tarea.objects.all().order_by('id')    
+        elif 'Filtrar' in request.POST:
+                filtradoDesignado = request.POST.get('designadoFiltrado')    
+                tareasInformacion = tarea.objects.filter(usuario_designado=filtradoDesignado)
     return render(request,'gestion_tareas/dashboard.html',{
-        'tareas': lista_tareas,
+        'tareas': tareasInformacion,
         })
 
